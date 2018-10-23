304861391ff04cf40e21166057e59679ca016b59
Jeff <1015005300@qq.com>
2018-10-23 15:00:17 +0800

    add some clear comments

--- load_batch_images.py
+++ load_batch_images.py
@@ -8,7 +8,8 @@ def read_single(f):
     
 def load_batch_images(data_dir):
     '''
-    Load all images in a folder and convert to numpy array, make sure all images are the same size. 
+    Load all images in a folder and convert to numpy array, 
+    Note: make sure all images are the same size. 
     Supports jpg and png format.
     '''
     from skimage import io as skio
@@ -18,7 +19,7 @@ def load_batch_images(data_dir):
     try:
         image_collection = skio.concatenate_images(image_collection)
     except:
-        print('All image sizes are not equal, \
-              return ImageCollection object instead of numpy array!!!')
+        print('''All image sizes are not equal, \
+              return ImageCollection object instead of numpy array!!!''')
     return image_collection
     
